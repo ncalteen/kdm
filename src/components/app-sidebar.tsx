6023787638789db33605a7519e560a839e206a50
Nick Alteen <ncalteen@github.com>
2025-05-16 20:49:38 -0400

    Reduce sidebar width

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -94,7 +94,7 @@ const footer = [
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props}>
+    <Sidebar collapsible="icon" {...props} className="w-[150px]">
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
