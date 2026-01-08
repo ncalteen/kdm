3685aeb9964b477f9dcbdcb61d074c55ad60b069
Nick Alteen <ncalteen@github.com>
2026-01-08 12:36:28 -0500

    feat: add new wanderers and schema for wanderer data

---
+++ src/lib/wanderers/candy-cola.ts
@@ -0,0 +1,29 @@
+import { Wanderer } from '@/schemas/wanderer'
+
+export const CANDY_COLA: Wanderer = {
+  abilitiesAndImpairments: ['Candypop', 'Veteran'],
+  accuracy: 0,
+  arc: true,
+  courage: 2,
+  disposition: 0,
+  evasion: 0,
+  fightingArts: ['Phantom Friend'],
+  huntXP: 0,
+  huntXPRankUp: [1, 3, 5, 12],
+  insanity: 0,
+  luck: 1,
+  lumi: 1,
+  movement: 6,
+  name: 'Candy & Cola',
+  permanentInjuries: [],
+  rareGear: ['Gladiator Garb', 'Sharpened Heel'],
+  speed: 0,
+  strength: 0,
+  survival: 3,
+  systemicPressure: 1,
+  timeline: {
+    2: ['Wanderer - Candy & Cola']
+  },
+  torment: 1,
+  understanding: 1
+}
---
+++ src/lib/wanderers/luck.ts
@@ -0,0 +1,29 @@
+import { Wanderer } from '@/schemas/wanderer'
+
+export const LUCK: Wanderer = {
+  abilitiesAndImpairments: ['Servant of Fate', 'Veteran'],
+  accuracy: -1,
+  arc: true,
+  courage: 5,
+  disposition: 0,
+  evasion: 0,
+  fightingArts: [],
+  huntXP: 0,
+  huntXPRankUp: [1, 5, 9, 13],
+  insanity: 7,
+  luck: 1,
+  lumi: 2,
+  movement: 5,
+  name: 'Luck',
+  permanentInjuries: ['headBlind'],
+  rareGear: ["Luck's Cloak", 'Natural Cardinal Staff'],
+  speed: 0,
+  strength: 0,
+  survival: 7,
+  systemicPressure: 0,
+  timeline: {
+    23: ['Wanderer - Luck']
+  },
+  torment: 1,
+  understanding: 5
+}
